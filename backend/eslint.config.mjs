0d7fc36e995cc440ca535ed160c9f781a8211185
pampaka <pampakadev@yandex.ru>
2024-10-06 10:24:12 +0300

    update backend eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -8,13 +8,17 @@ export default tseslint.config(
 		extends: [js.configs.recommended, ...tseslint.configs.recommended],
 		languageOptions: {
 			ecmaVersion: 2020,
-			globals: globals.node
+			globals: { ...globals.node, ...globals.jest }
 		},
 		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{ vars: "all", args: "none", caughtErrors: "none" }
+			],
 			semi: ["error", "always"],
 			quotes: ["error", "double", { avoidEscape: true }],
 			"no-undef": "error",
-			"no-unused-vars": ["error", { vars: "all", args: "none", caughtErrors: "none" }],
+			"no-unused-vars": "off",
 			"no-constant-condition": "off",
 			"no-empty": ["error", { allowEmptyCatch: true }],
 			"no-console": "warn"
