04594171c21fa3e224b6425b0e99a0283143b207
pampaka <pampakadev@yandex.ru>
2024-10-10 16:05:56 +0300

    fix components module

--- backend/src/components/components.module.ts
+++ backend/src/components/components.module.ts
@@ -3,12 +3,12 @@ import { ComponentsService } from "./components.service";
 import { ComponentsController } from "./components.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Component, ComponentSchema } from "./component.schema";
-import { BoardsService } from "boards/boards.service";
+import { BoardsModule } from "boards/boards.module";
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([{ schema: ComponentSchema, name: Component.name }]),
-		BoardsService
+		BoardsModule
 	],
 	providers: [ComponentsService],
 	controllers: [ComponentsController]
